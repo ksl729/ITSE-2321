5cdcb4916b220136c9bda5ea2c92297bc39bb204
Karly L. <ksleach21@amarillocollege.com>
2026-09-08 16:23:46 -0500

    Week 3 Lecture Notes
    Week 3 Lecture Notes

---
+++ Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Week3Lecture;
+
+// Create the class 'Sandwich'
+internal class Sandwich
+{
+
+    string breadType;
+    int numberOfSlices;
+    string protein; //Type of protein
+    int numberOfProtein; // Number of slices of protein
+    bool hasCheese;
+    string condiment;
+    bool isToasted;
+    string size;
+    string fruit;
+    string vegetable;
+    bool hasDecoration;
+
+
+    // This is the conctructor
+    // It is a special method that is called when an object of the class is created
+    // Ex. Sandwich mySandwich = new Sandwich("Turkey Sandwich", "Wheat", "Large");
+    public Sandwich(string SandwichName, string BreadType, string Size)
+    {// Signature of the constructor is NOT the same as the class name
+        sandwichName = SandwichName;
+        breadType = BreadType;
+        numberOfSlices = 2;
+        protein = "N/A";
+        numberOfProtein = 0;
+        hasCheese = false;
+        condiment = "N/A";
+        isToasted = false;
+        size = Size;
+        fruit = "N/A";
+        vegetable = "N/A";
+        hasDecoration = false;
+    }
+
+    // This is the conctructor
+    // It is a special method that is called when an object of the class is created
+    public Sandwich(string sandwichName, string breadType, string size, string protein, int numberOfProtein, bool hasCheese, string condiment, bool isToasted, string fruit, string vegetable, bool hasDecoration)
+    {// Signature of the constructor has more of the attributes listed in the class
+        this.sandwichName = sandwichName;
+        this.breadType = breadType;
+        this.numberOfSlices = 2;
+        this.protein = "N/A";
+        this.numberOfProtein = 0;
+        this.hasCheese = false;
+        this.condiment = "N/A";
+        this.isToasted = false;
+        this.size = size;
+        this.fruit = "N/A";
+        this.vegetable = "N/A";
+        this.hasDecoration = false;
+    }
+
+    // This is a method that will return the value of the attribute sandwichName
+    public string GetName()
+    {
+        return sandwichName;
+    }
+
+    // The void means this will not return anything, it will just set the value of the attribute
+    public void SetName(string newValue)
+    {
+        sandwichName = newValue;
+    }
+
+    public void SetCheese(bool newValue)
+    {
+        hasCheese = newValue;
+    }   
+
+    // Prints out all available attributes of the sandwich, even if they are not set
+    public override string ToString()
+    {
+        return "Sandwich Name: " + sandwichName +
+               "\nBread Type: " + breadType +
+               "\nNumber of Slices: " + numberOfSlices +
+               "\nType of Protein: " + protein +
+               "\nNumber of Protein Slices: " + numberOfProtein +
+               "\nWith Cheese?: " + hasCheese;
+               
+               ;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,92 +1,16 @@
-﻿using System;
-
-namespace Lab2_KLeach
-//  Name: Karly Leach
-//  Date: 9/3/2026
-//  Class: ITSE 2321
-//  Description: This program demonstrates the bubble sort algorithm on both a 1D and a 2D array of integers
+﻿namespace Week3Lecture
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("First Array Sort");
-            // Original array of integers
-            int[] originalArray = { 12, 7, 4, 27, 1, 13, 19, 6, 30, 9, 8 };
+        {   // Create an object of the class Sandwich
+            Sandwich mySandwich = new Sandwich("Italian Herbs and Cheese", "Italian Herb", "12 Inches"); // Could call with this line with no parameters because of the default constructor
 
-            // Using the bubble sort method
-            int[] bubbleSortedArray = BubbleSort(originalArray);
-            Console.WriteLine("Bubble Sorted Array: " + string.Join(", ", bubbleSortedArray));
+            mySandwich.SetCheese(true); // Set the cheese property to true. Does not return anything because it is a void method
 
-            Console.WriteLine("Second Array Sort");
-            SortAndPrint2DMatrix();
+            Console.WriteLine(mySandwich.ToString()); // Call the ToString method to display the sandwich information
+
         }
 
-        static int[] BubbleSort(int[] array)
-        {
-            int n = array.Length;
-            for (int i = 0; i < n - 1; i++)
-            {
-                for (int j = 0; j < n - i - 1; j++)
-                {
-                    if (array[j] > array[j + 1])
-                    {
-                        int temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-            return array; // Returns the sorted array
-        }
-
-        static void SortAndPrint2DMatrix()
-        {
-            int[,] matrix = {
-                { 54, 3, 21, 50, 57, 7, 73, 24, 85, 79 },
-                { 33, 11, 19, 33, 51, 72, 80, 6, 78, 25 },
-                { 1, 65, 4, 86, 71, 52, 16, 77, 29, 13 },
-                { 23, 44, 90, 70, 28, 17, 53, 64, 62, 83 },
-                { 55, 98, 22, 89, 34, 75, 56, 58, 27, 35 },
-                { 87, 88, 8, 61, 74, 14, 66, 20, 59, 81 },
-                { 18, 76, 2, 5, 26, 84, 15, 82, 9, 60 }
-            };
-
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-            int twoDArraySize = rows * cols;
-
-            int[] oneDArray = new int[twoDArraySize];
-            int index = 0;
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < cols; c++)
-                {
-                    oneDArray[index++] = matrix[r, c];
-                }
-            }
-
-            int[] bubbleSortedArray2D = BubbleSort(oneDArray);
-            Console.WriteLine("Bubble Sorted 2D Array: " + string.Join(", ", bubbleSortedArray2D));
-
-            index = 0;
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < cols; c++)
-                {
-                    matrix[r, c] = bubbleSortedArray2D[index++];
-                }
-            }
-
-            Console.WriteLine("Bubble Sorted 2D Array (Original Structure):");
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < cols; c++)
-                {
-                    Console.Write(matrix[r, c] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
     }
 }
